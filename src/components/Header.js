1f4340d58639ca51d0b68b5b673ddb76378124a6
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-08-01 19:59:19 +0300

    creating first branch

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+
+
+render() {
+
+return (
+
+    <h1>This is the header</h1>
+)
+
+
+}
+
+
+
+
+
+}
+
+export default Header;
