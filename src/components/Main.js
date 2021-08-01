c957ffede873f9f157a15e8939b48f24f3ff9958
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-08-01 20:34:25 +0300

    added class02

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HornedBeasts from "./HornedBeasts";
+import OrginalArray from "./JSON"
 
 let title1 = "Cat";
 let title2 = "Dog"
@@ -12,13 +13,19 @@ let desc2 = "This a dog pic"
 
 class Main extends React.Component {
   render() {
+
+
     return (
-      <div>
-        <HornedBeasts newName={title1} newImg = {"https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697"} newDesc ={desc1} />
-        <HornedBeasts newName={title2} newImg = {"https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg"} newDesc ={desc2}/>
-      </div>
+      let newArr = OrginalArray.map((item => {
+
+      }))
+      return newArr
     );
   }
 }
 
 export default Main;
+
+<HornedBeasts  title img description />
+
+
