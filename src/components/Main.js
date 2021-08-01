d9e85e1f5d8c2252eee0f0a9c2ce31a92397d35f
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-08-02 00:53:17 +0300

    creating class02

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,26 @@
 import React from "react";
 import HornedBeasts from "./HornedBeasts";
-import OrginalArray from "./JSON"
+import Data from "./Data.json";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-let title1 = "Cat";
-let title2 = "Dog"
+// let title1 = "Cat";
+// let title2 = "Dog"
 
-
-let desc1 = "this is a cat pic"
-let desc2 = "This a dog pic"
-
-
+// let desc1 = "this is a cat pic"
+// let desc2 = "This a dog pic"
 
 class Main extends React.Component {
   render() {
-
-
-    return (
-      let newArr = OrginalArray.map((item => {
-
-      }))
-      return newArr
-    );
+    return Data.map((item) => {
+      return (
+        <HornedBeasts
+          title={item.title}
+          description={item.description}
+          image_url={item.image_url}
+        />
+      );
+    });
   }
 }
 
 export default Main;
-
-<HornedBeasts  title img description />
-
-
